c2d29231ba382a3f3653a32b3f507b9c1eb50fb1
aguilalv <alvaro.aguilar.lopez@gmail.com>
2020-02-13 18:05:03 +0000

    Added code for parser to find link to next page and add to list of pages to parse

--- REScrap/spiders/idealista.py
+++ REScrap/spiders/idealista.py
@@ -4,8 +4,14 @@ import scrapy
 
 class IdealistaSpider(scrapy.Spider):
     name = 'idealista'
-    allowed_domains = ['https://www.idealista.com/venta-viviendas/benicasimbenicassim/heliopolis-eurosol/']
-    start_urls = ['http://https://www.idealista.com/venta-viviendas/benicasimbenicassim/heliopolis-eurosol//']
+    #allowed_domains = ['idealista.com']
+    start_urls = ['https://www.idealista.com/venta-viviendas/benicasimbenicassim/heliopolis-eurosol/']
+
+#    def parse_list(self,response):
+
 
     def parse(self, response):
-        pass
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            print(f'-----> {next_page}') 
+            yield response.follow(next_page, callback=self.parse)
