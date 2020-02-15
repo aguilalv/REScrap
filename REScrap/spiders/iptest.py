f61b7fcfda6041e1efd11ec139e36b27fc5b0fc8
aguilalv <alvaro.aguilar.lopez@gmail.com>
2020-02-15 23:04:10 +0000

    Added code to extract user agent in iptest spider

--- REScrap/spiders/iptest.py
+++ REScrap/spiders/iptest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
 
 class IptestSpider(scrapy.Spider):
     name = 'iptest'
@@ -9,4 +9,10 @@ class IptestSpider(scrapy.Spider):
 
     def parse(self, response):
         ip = response.css("tr input::attr(value)").get()
+        
+        ua_text = response.css("table")[1].css("tr")[2].css("td::text").get()
+        ua = re.search('Your browser User Agent: (.*)', ua_text, re.IGNORECASE).group(1)
+
         print(f"------------> {ip}")
+        print(f"------------> {ua}")
+    
